2f9df9b6069210ce877295637dac139cbc585cf0
Alexander Hernández Herrera <aherrera@itcoint.com>
2021-04-12 17:47:16 -0600

    Agregar archivos de proyecto.

---
+++ demolun/Controllers/demoController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+
+namespace demolun.Controllers
+{
+    public class demoController : Controller
+    {
+        // GET: demo
+        BIBLIOTECAEntities db = new BIBLIOTECAEntities();
+        public ActionResult Index()
+        {
+
+            return View(db.LIBRO.ToList()); 
+        }
+
+        [HttpPost]
+        public ActionResult Index(DateTime start,DateTime end)
+        {
+
+            return View(db.GetFuntion(start,end));
+        }
+    }
+}
